0d6861c3fa2567aebc7f0e435cfb014260d4c782
YariMil <secondgameguy@icloud.com>
2026-09-24 08:07:05 -0700

    (GP-2.1): Program Can Initialize Repository

--- Git.java
+++ Git.java
@@ -3,14 +3,25 @@ import java.io.IOException;
 
 public class Git {
 
-    public void init() {
+    public static void main(String[] args) {
+        init();
+    }
+
+    public static void init() {
         try {
             File gitRoot = new File("git");
-            boolean rootExists = gitRoot.mkdir();
+            boolean rootExists = !gitRoot.mkdir();
             File gitObjects = new File("git/objects");
-            boolean objectsExists = gitObjects.mkdir();
+            boolean objectsExists = !gitObjects.mkdir();
             File gitIndex = new File("git/index");
-            boolean indexExists = gitIndex.createNewFile();
+            boolean indexExists = !gitIndex.createNewFile();
+            File gitHead = new File("git/HEAD");
+            boolean headExists = !gitHead.createNewFile();
+            if (rootExists && objectsExists && indexExists && headExists) {
+                System.out.println("Git Repository already exists.");
+            } else {
+                System.out.println("Git Repository created.");
+            }
         } catch (IOException e) {
             System.out.println("File error: " + e.getMessage());
         }
